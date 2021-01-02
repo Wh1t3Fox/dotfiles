ac59a3c672f39d42220973ec24d67955ac36465b
wh1t3fox <dev@exploit.design>
2021-01-02 08:15:07 -0500

    fix multi monitor detection

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,11 +8,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITORS=$(xrandr --query | grep ' connected ' | awk -F' ' '{print $1}')
+export MONITORS=($(xrandr --query | grep ' connected ' | awk -F' ' '{print $1}'))
 
-if [ "${#MONITORS[@]}" -eq 1 ]; then
-    polybar -c ~/.config/polybar/config.ini single &
+if [[ "${#MONITORS[@]}" -eq 1 ]]; then
+    polybar -c ~/.config/polybar/config.ini single > /dev/null 2>&1 &!
 else
-    polybar -c ~/.config/polybar/config.ini left &
-    polybar -c ~/.config/polybar/config.ini right &
+    polybar -c ~/.config/polybar/config.ini left > /dev/null 2>&1 &!
+    polybar -c ~/.config/polybar/config.ini right > /dev/null 2>&1 &!
 fi
