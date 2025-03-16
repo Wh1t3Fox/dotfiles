6f57950886b79a0ab02962fe46396922a33aeff5
wh1t3fox <dev@exploit.design>
2025-03-16 09:57:30 -0400

    update icon, and support for laptop and desktop

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -15,8 +15,4 @@ if [[ "${#MONITORS[@]}" -eq 1 ]]; then
 elif [[ "${#MONITORS[@]}" -eq 2 ]]; then
     polybar -c ~/.config/polybar/config.ini left > /dev/null 2>&1 &!
     polybar -c ~/.config/polybar/config.ini right > /dev/null 2>&1 &!
-elif [[ "${#MONITORS[@]}" -eq 3 ]]; then
-    polybar -c ~/.config/polybar/config.ini left > /dev/null 2>&1 &!
-    polybar -c ~/.config/polybar/config.ini right > /dev/null 2>&1 &!
-    polybar -c ~/.config/polybar/config.ini bottom > /dev/null 2>&1 &!
 fi
